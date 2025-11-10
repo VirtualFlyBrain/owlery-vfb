b8e565fddd95cb6fb6412b1347fb8e65b296b912
Rob Court <robbie.court@gmail.com>
2025-11-10 18:56:47 +0000

    Add environment variable defaults for OWLURL and LOGLEVEL in startup script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM phenoscape/owlery
 
-MAINTAINER Robbie - Virtual Fly Brain <rcourt@ed.ac.uk>
+LABEL maintainer="Robbie - Virtual Fly Brain <rcourt@ed.ac.uk>"
 
-ENV OWLURL="http://VirtualFlyBrain.org/data/VFB/OWL/owlery-live.owl"
+ENV OWLURL=http://VirtualFlyBrain.org/data/VFB/OWL/owlery-live.owl
 ENV LOGLEVEL=INFO
 
 COPY application.conf /srv/conf/application.conf
--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 echo "Loading OWLERY"
+
+# Set defaults if environment variables are not set
+OWLURL=${OWLURL:-"http://www.virtualflybrain.org/owl/vfb.owl"}
+LOGLEVEL=${LOGLEVEL:-"INFO"}
+
 sed -i "s|http://www.virtualflybrain.org/owl/vfb.owl|${OWLURL}|g" /srv/conf/application.conf
 sed -i "s|INFO|${LOGLEVEL}|g" /srv/conf/application.conf
 
 cat /srv/conf/application.conf
 
-/usr/share/owlery/bin/owlery -Dowlery.port=8080 -Dowlery.host=0.0.0.0 -Dconfig.file=/srv/conf/application.conf
+/usr/share/owlery/bin/owlery -Dowlery.port=8080 -Dowlery.host=0.0.0.0 -Dconfig.file=/srv/conf/application.conf -Dorg.slf4j.simpleLogger.defaultLogLevel=${LOGLEVEL}
